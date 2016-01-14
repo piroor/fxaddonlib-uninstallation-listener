7b866dcd1244532c7c12ac1dc74b7893528420c7
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2016-01-14 23:33:12 +0900

    Simplify: drop support for too old Firefox/Thunderbird

--- UninstallationListener.js
+++ UninstallationListener.js
@@ -1,46 +1,23 @@
-/*
- Uninstallation Listener Library
+/**
+ * @fileOverview Uninstallation Listener Library
+ * @author       YUKI "Piro" Hiroshi
+ * @version      3
+ *
+ * @license
+ *   The MIT License, Copyright (c) 2009-2016 YUKI "Piro" Hiroshi.
+ *   https://github.com/piroor/fxaddonlib-uninstallation-listener/blob/master/license.txt
+ * @url https://github.com/piroor/fxaddonlib-uninstallation-listener
+ *
+ * Usage:
+ *   new UninstallationListener({
+ *     id : 'test@exmaple.com',
+ *    onuninstalled : function() { ... },
+ *     ondisabled : function() { ... }
+ *   });
+ */
 
- Usage:
-   new window['piro.sakura.ne.jp'].UninstallationListener({
-     id : 'test@exmaple.com',
-     onuninstalled : function() { ... },
-     ondisabled : function() { ... }
-   });
+var EXPORTED_SYMBOLS = ['UninstallationListener'];
 
- license: The MIT License, Copyright (c) 2009-2011 YUKI "Piro" Hiroshi
- original:
-   http://github.com/piroor/fxaddonlib-uninstallation-listener
-*/
-
-/* To work as a JS Code Module  */
-if (typeof window == 'undefined' ||
-	(window && typeof window.constructor == 'function')) {
-	this.EXPORTED_SYMBOLS = ['UninstallationListener'];
-
-	// If namespace.jsm is available, export symbols to the shared namespace.
-	// See: http://github.com/piroor/fxaddonlibs/blob/master/namespace.jsm
-	try {
-		let ns = {};
-		Components.utils.import('resource://my-modules/namespace.jsm', ns);
-		/* var */ window = ns.getNamespaceFor('piro.sakura.ne.jp');
-	}
-	catch(e) {
-		window = {};
-	}
-}
-
-(function() {
-	const currentRevision = 2;
-
-	if (!('piro.sakura.ne.jp' in window)) window['piro.sakura.ne.jp'] = {};
-
-	var loadedRevision = 'UninstallationListener' in window['piro.sakura.ne.jp'] ?
-			window['piro.sakura.ne.jp'].UninstallationListener.prototype.revision :
-			0 ;
-	if (loadedRevision && loadedRevision > currentRevision) {
-		return;
-	}
 
 	const Cc = Components.classes;
 	const Ci = Components.interfaces;
@@ -48,61 +25,12 @@ if (typeof window == 'undefined' ||
 	const ObserverService = Cc['@mozilla.org/observer-service;1']
 							.getService(Ci.nsIObserverService);
 
-	var AddonManager;
-	try {
-		let ns = {};
-		Components.utils.import('resource://gre/modules/AddonManager.jsm', ns);
-		AddonManager = ns.AddonManager;
-	}
-	catch(e) {
-	}
+	var { AddonManager } = Components.utils.import('resource://gre/modules/AddonManager.jsm', {});
 
-	window['piro.sakura.ne.jp'].UninstallationListener = function(aArguments) {
+	var UninstallationListener = function(aArguments) {
 		this.init(aArguments);
 	};
-	window['piro.sakura.ne.jp'].UninstallationListener.prototype = {
-		revision : currentRevision,
-
-		// for Firefox 3.6 or older
-		observe : function(aSubject, aTopic, aData)
-		{
-			switch (aTopic)
-			{
-				case 'em-action-requested':
-					switch (aData)
-					{
-						case 'item-uninstalled':
-							if (this.isTargetExtension(aSubject))
-								this.toBeUninstalled = true;
-							break;
-						case 'item-cancel-action':
-							if (this.isTargetExtension(aSubject)) {
-								this.toBeUninstalled = false;
-								this.toBeDisabled = false;
-							}
-							break;
-						case 'item-disabled':
-							if (this.isTargetExtension(aSubject))
-								this.toBeDisabled = true;
-							break;
-						case 'item-enabled':
-							if (this.isTargetExtension(aSubject))
-								this.toBeDisabled = false;
-							break;
-					}
-					return;
-
-				case 'quit-application-granted':
-					this.destroy();
-					return;
-			}
-		},
-		isTargetExtension : function(aSubject)
-		{
-			return (aSubject instanceof Ci.nsIUpdateItem) && (aSubject.id == this.id);
-		},
-
-		// for Firefox 4 or later
+	UninstallationListener.prototype = {
 		onEnabling : function(aAddon, aRestartRequired) {},
 		onEnabled : function(aAddon) {},
 		onDisabling : function(aAddon, aRestartRequired) {
@@ -153,12 +81,7 @@ if (typeof window == 'undefined' ||
 				this.ondisabled = aArguments.ondisabled;
 			}
 
-			if (AddonManager) { // Firefox 4 or later
-				AddonManager.addAddonListener(this);
-			}
-			else {
-				ObserverService.addObserver(this, 'em-action-requested', false);
-			}
+			AddonManager.addAddonListener(this);
 			ObserverService.addObserver(this, 'quit-application-granted', false);
 		},
 
@@ -172,17 +95,7 @@ if (typeof window == 'undefined' ||
 				this.ondisabled();
 			delete this.ondisabled;
 
-			if (AddonManager) { // Firefox 4 or later
-				AddonManager.removeAddonListener(this);
-			}
-			else {
-				ObserverService.removeObserver(this, 'em-action-requested');
-			}
+			AddonManager.removeAddonListener(this);
 			ObserverService.removeObserver(this, 'quit-application-granted');
 		}
 	};
-})();
-
-if (window != this) { // work as a JS Code Module
-	this.UninstallationListener = window['piro.sakura.ne.jp'].UninstallationListener;
-}
