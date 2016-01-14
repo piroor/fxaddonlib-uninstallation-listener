ad99c237d58c759abf1f30c16cfc8fb9faa64e26
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2016-01-14 23:33:47 +0900

    Fix indent

--- UninstallationListener.js
+++ UninstallationListener.js
@@ -19,83 +19,83 @@
 var EXPORTED_SYMBOLS = ['UninstallationListener'];
 
 
-	const Cc = Components.classes;
-	const Ci = Components.interfaces;
+const Cc = Components.classes;
+const Ci = Components.interfaces;
 
-	const ObserverService = Cc['@mozilla.org/observer-service;1']
-							.getService(Ci.nsIObserverService);
+const ObserverService = Cc['@mozilla.org/observer-service;1']
+						.getService(Ci.nsIObserverService);
 
-	var { AddonManager } = Components.utils.import('resource://gre/modules/AddonManager.jsm', {});
+var { AddonManager } = Components.utils.import('resource://gre/modules/AddonManager.jsm', {});
 
-	var UninstallationListener = function(aArguments) {
-		this.init(aArguments);
-	};
-	UninstallationListener.prototype = {
-		onEnabling : function(aAddon, aRestartRequired) {},
-		onEnabled : function(aAddon) {},
-		onDisabling : function(aAddon, aRestartRequired) {
-			if (aAddon.id == this.id) {
-				this.toBeDisabled = true;
-			}
-		},
-		onDisabled : function(aAddon)
-		{
-			if (aAddon.id == this.id) {
-				if (this.ondisabled) this.ondisabled();
-				delete this.ondisabled;
-			}
-		},
-		onInstalling : function(aAddon, aRestartRequired) {},
-		onInstalled : function(aAddon) {},
-		onUninstalling : function(aAddon, aRestartRequired) {
-			if (aAddon.id == this.id) {
-				this.toBeUninstalled = true;
-			}
-		},
-		onUninstalled : function(aAddon)
-		{
-			if (aAddon.id == this.id) {
-				if (this.onuninstalled) this.onuninstalled();
-				delete this.onuninstalled;
-			}
-		},
-		onOperationCancelled : function(aAddon)
-		{
-			if (aAddon.id == this.id) {
-				this.toBeDisabled = false;
-				this.toBeUninstalled = false;
-			}
-		},
-		onPropertyChanged : function(aAddon, aProperties) {},
-
-		init : function(aArguments)
-		{
-			if (!aArguments) return;
-
-			this.id = (typeof aArguments == 'string') ? aArguments : aArguments.id ;
-
-			this.toBeUninstalled = false;
-			this.toBeDisabled = false;
-			if (typeof aArguments == 'object') {
-				this.onuninstalled = aArguments.onuninstalled;
-				this.ondisabled = aArguments.ondisabled;
-			}
-
-			AddonManager.addAddonListener(this);
-			ObserverService.addObserver(this, 'quit-application-granted', false);
-		},
-
-		destroy : function()
-		{
-			if (this.toBeUninstalled && this.onuninstalled)
-				this.onuninstalled();
-			delete this.onuninstalled;
-
-			if (this.toBeDisabled && this.ondisabled)
-				this.ondisabled();
-			delete this.ondisabled;
-
-			AddonManager.removeAddonListener(this);
-			ObserverService.removeObserver(this, 'quit-application-granted');
+var UninstallationListener = function(aArguments) {
+	this.init(aArguments);
+};
+UninstallationListener.prototype = {
+	onEnabling : function(aAddon, aRestartRequired) {},
+	onEnabled : function(aAddon) {},
+	onDisabling : function(aAddon, aRestartRequired) {
+		if (aAddon.id == this.id) {
+			this.toBeDisabled = true;
 		}
-	};
+	},
+	onDisabled : function(aAddon)
+	{
+		if (aAddon.id == this.id) {
+			if (this.ondisabled) this.ondisabled();
+			delete this.ondisabled;
+		}
+	},
+	onInstalling : function(aAddon, aRestartRequired) {},
+	onInstalled : function(aAddon) {},
+	onUninstalling : function(aAddon, aRestartRequired) {
+		if (aAddon.id == this.id) {
+			this.toBeUninstalled = true;
+		}
+	},
+	onUninstalled : function(aAddon)
+	{
+		if (aAddon.id == this.id) {
+			if (this.onuninstalled) this.onuninstalled();
+			delete this.onuninstalled;
+		}
+	},
+	onOperationCancelled : function(aAddon)
+	{
+		if (aAddon.id == this.id) {
+			this.toBeDisabled = false;
+			this.toBeUninstalled = false;
+		}
+	},
+	onPropertyChanged : function(aAddon, aProperties) {},
+
+	init : function(aArguments)
+	{
+		if (!aArguments) return;
+
+		this.id = (typeof aArguments == 'string') ? aArguments : aArguments.id ;
+
+		this.toBeUninstalled = false;
+		this.toBeDisabled = false;
+		if (typeof aArguments == 'object') {
+			this.onuninstalled = aArguments.onuninstalled;
+			this.ondisabled = aArguments.ondisabled;
+		}
+
+		AddonManager.addAddonListener(this);
+		ObserverService.addObserver(this, 'quit-application-granted', false);
+	},
+
+	destroy : function()
+	{
+		if (this.toBeUninstalled && this.onuninstalled)
+			this.onuninstalled();
+		delete this.onuninstalled;
+
+		if (this.toBeDisabled && this.ondisabled)
+			this.ondisabled();
+		delete this.ondisabled;
+
+		AddonManager.removeAddonListener(this);
+		ObserverService.removeObserver(this, 'quit-application-granted');
+	}
+};
